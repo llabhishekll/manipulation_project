48276163dddd157dab41895c80ff123de4e86a98
Abhishek Verma <abhishek_verma@hotmail.com>
2024-01-26 11:34:19 +0000

    feat: added pick and palce node for real

--- moveit2_scripts/launch/pick_and_place.launch.py
+++ moveit2_scripts/launch/pick_and_place.launch.py
@@ -11,7 +11,8 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration("use_sim_time", default="True")
 
     # moveit config
-    moveit_config = MoveItConfigsBuilder("name", package_name="my_moveit_config").to_moveit_configs()
+    moveit_sim_config = MoveItConfigsBuilder("name", package_name="my_moveit_config").to_moveit_configs()
+    moveit_real_config = MoveItConfigsBuilder("name", package_name="real_moveit_config").to_moveit_configs()
 
     # return
     return LaunchDescription(
@@ -23,12 +24,25 @@ def generate_launch_description():
                 name="pick_and_place_sim_node",
                 output="screen",
                 parameters=[
-                    moveit_config.to_dict(),
+                    moveit_sim_config.to_dict(),
                     {"trajectory_execution.allowed_execution_duration_scaling": 2.0,},
                     {"publish_robot_description_semantic": True},
                     {"use_sim_time": True},
                 ],
                 condition=IfCondition(use_sim_time),
+            ),
+            Node(
+                package="moveit2_scripts",
+                executable="pick_and_place_real",
+                name="pick_and_place_real_node",
+                output="screen",
+                parameters=[
+                    moveit_real_config.to_dict(),
+                    {"trajectory_execution.allowed_execution_duration_scaling": 5.0,},
+                    {"publish_robot_description_semantic": True},
+                    {"use_sim_time": False},
+                ],
+                condition=UnlessCondition(use_sim_time),
             )
         ]
     )
